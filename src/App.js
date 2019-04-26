12d6f2d06cd3b6dbcc89f495c8209e31551b9880
Ashish Garg <mragarg@gmail.com>
2019-04-26 09:35:54 -0400

    Adds Game of Thrones Logo and prevents spinning animation

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
+import gotLogo from './images/gotLogo.png';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <img src={gotLogo} className="App-logo" alt="logo" />
+      <h3>
+        Character App
+      </h3>
     </div>
   );
 }
